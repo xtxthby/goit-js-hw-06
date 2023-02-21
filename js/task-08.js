64532053d19b6a00cc7bbff1f80c8848bafa522a
Yurii <116266510+xtxthby@users.noreply.github.com>
2023-02-21 16:27:35 +0200

    add-8b

--- js/task-08.js
+++ js/task-08.js
@@ -1,30 +1,91 @@
-const formEl = document.querySelector('.login-form');
+// const formEl = document.querySelector('.login-form');
 
-formEl.addEventListener('submit', onFormSubmit);
+// formEl.addEventListener('submit', onFormSubmit);
 
+
+// function onFormSubmit(event) {
+//     event.preventDefault();
+    
+//     const {email, password} = event.currentTarget.elements;
+
+//     if (email.value === '' || password.value === '') {
+//         alert("error: Всі поля повинні бути заповнені !!!!!");
+//         return;
+//     } 
+//     const formData = {
+//         email: email.value,
+//         password: password.value,
+//     }
+//     console.log(formData);
+   
+//     event.currentTarget.reset();
+   
+    
+    
+// }
+
+
+
+
+// спосіб 2 треба доробити
+const refs = {
+    form: document.querySelector('.login-form'),
+    inputs: document.querySelectorAll('input'),
+};
+ 
+
+refs.form.addEventListener('submit', onFormSubmit );
 
 function onFormSubmit(event) {
     event.preventDefault();
     
-    const {email, password} = event.currentTarget.elements;
+    refs.inputs.forEach(input => {
 
-    if (email.value === '' || password.value === '') {
-        alert("error: Всі поля повинні бути заповнені !!!!!");
-        return;
-    } 
-    const formData = {
-        email: email.value,
-        password: password.value,
-    }
-    console.log(formData);
-   
+        if (input.value === '') {
+            alert("error: Усі поля повинні бути заповнені!!!");
+            return;
+        }
+    });
+    
+    const formData = new FormData(event.currentTarget);
+    formData.forEach((value, name) => {
+        console.log(`name :`, name );
+        console.log(`value :`, value);
+    });
+    
+    
     event.currentTarget.reset();
-   
-    
-    
 }
 
 
 
 
+//   const formData =  new FormData(event.currentTarget);
 
+//   console.log(formData);
+//    
+//   formData.forEach((value, name) => {
+//     console.log(`name`, name );
+//     console.log(`value`, value);
+//   });
+
+//   document.getElementById("login-form").reset();
+// }
+// function validate_form ()
+// {
+//     valid = true;
+
+//         if ( document.login-form.email.password.value == "" )
+//         {
+//                 alert ( "Все поля должны быть заполнены." );
+//                 valid = false;
+//         }
+
+//         return valid;
+// }
+// inputs.foreach(input => {
+//     if (input.value.trim() === '') { 
+//     alert("error")
+//     }
+// }
+//     const inputs = document.querySelectorAll('input');
--- js/task-10.js
+++ js/task-10.js
@@ -13,31 +13,109 @@ function getRandomHexColor() {
 }
 
 
+// let inputValue = 0;
+// function toogleValue(event) {
+//   inputValue = event.currentTarget.value;
+// }
+
+
+// function createBoxes(amount) {
+//   let divArray = [];
+//   let sizeSquare = 30;
+//   for (let i = 0; i < amount; i += 1) {
+//     sizeSquare += 10;
+//     const divTag = document.createElement('div');
+//     divTag.classList = 'item';
+//     divTag.style.height = `${sizeSquare}px`;
+//     divTag.style.width = `${sizeSquare}px`;
+//     divTag.style.marginRight = '30px';
+//     divTag.style.marginBottom = '30px';
+//     divTag.style.backgroundColor = getRandomHexColor();
+//     divArray.push(divTag);
+//   }
+//   return divBoxes.append(...divArray);
+// }
+
+
+// function destroyBoxesMarkup() {
+//   inputNumber.value = '';
+//   return (divBoxes.innerHTML = '');
+// }
+
+
+
+
+
+// --- next variant -------------------------------------------
+
+// // let inputValue = 0;
+// // function toogleValue(event) {
+// //   inputValue = event.currentTarget.value;
+// // }
+// createBtn.addEventListener('click', () => {
+//   //console.log(inputNumberEl.value);
+//   if (
+//     Number(inputNumber.value.trim()) > Number(inputNumber.max) ||
+//     Number(inputNumber.value.trim()) < Number(inputNumber.min)
+//   ) {
+//     alert('Please enter number from 1 to 100');
+//   } else {
+//     createBoxes(inputNumber.value.trim());
+//   }
+//   inputNumber.value = '';
+// });
+// destroyBtn.addEventListener('click', destroyBoxes);
+
+
+// function destroyBoxes() {
+//   inputNumber.value = '';
+//   divBoxes.innerHTML = '';
+// }
+
+// function createBoxes(amount) {
+//   let defaultSize = 30;
+//   const boxesArr = [];
+//   for (let i = 0; i < amount; i += 1) {
+//     defaultSize += 10 * i;
+//     const div = document.createElement('div');
+//     div.classList = 'item';
+//     div.style.width = `${defaultSize}px`;
+//     div.style.height = `${defaultSize}px`;
+//     div.style.marginRight = '30px';
+//     div.style.marginBottom = '30px';
+//     div.style.backgroundColor = getRandomHexColor();
+//     boxesArr.push(div);
+//     console.log('arr length', boxesArr.length);
+//   }
+//   return divBoxes.append(...boxesArr);
+// }
+
+
+
+
+
+
+// --- next variant function createBoxes ()--------------------
+
 let inputValue = 0;
 function toogleValue(event) {
   inputValue = event.currentTarget.value;
 }
-
-
 function createBoxes(amount) {
-  let divArray = [];
-  let sizeSquare = 30;
+  let size = 30;
+  const boxesArr = [];
   for (let i = 0; i < amount; i += 1) {
-    sizeSquare += 10;
-    const divTag = document.createElement('div');
-    divTag.classList = 'item';
-    divTag.style.height = `${sizeSquare}px`;
-    divTag.style.width = `${sizeSquare}px`;
-    divTag.style.marginRight = '30px';
-    divTag.style.marginBottom = '30px';
-    divTag.style.backgroundColor = getRandomHexColor();
-    divArray.push(divTag);
+    size += 10 * i;
+    const div = `<div class="item" style="display:block;
+    margin-right:30px;
+    argin-bottom: 30px;
+    background-color: ${getRandomHexColor()}; width: ${size}px;
+    height: ${size}px;"></div>`;
+    boxesArr.push(div);
   }
-  return divBoxes.append(...divArray);
+  divBoxes.insertAdjacentHTML('beforeend', boxesArr.join(''));
 }
-
-
-function destroyBoxesMarkup() {
+function destroyBoxes() {
   inputNumber.value = '';
-  return (divBoxes.innerHTML = '');
+  divBoxes.innerHTML = '';
 }
